14bea6825e6737bb039b1ab7f7fb6fbb5ccb75d3
Inim Andrew <inimandrew@gmail.com>
2021-09-23 15:04:36 +0100

    UI update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,16 +1,9 @@
 <nav class="navbar navbar-default navbar-static-top m-b-0">
     <div class="navbar-header">
-        <div class="top-left-part">
+        <div style="display: flex;align-items:center;justify-content:center" class="top-left-part">
             <!-- Logo -->
-            <a class="logo" href="{{route('admin_home')}}">
-                <!-- Logo icon image, you can use font-icon also --><b>
-                    <!--This is dark logo icon--><img src="{{url('assets/administrator/plugins/images/admin-logo.png')}}" alt="home" class="dark-logo" />
-                    <!--This is light logo icon--><img src="{{url('assets/administrator/plugins/images/admin-logo-dark.png')}}" alt="home" class="light-logo" />
-                </b>
-                <!-- Logo text image you can use text also --><span class="hidden-xs">
-                    <!--This is dark logo text--><img src="{{url('assets/administrator/plugins/images/admin-text.png')}}" alt="home" class="dark-logo" />
-                    <!--This is light logo text--><img src="{{url('assets/administrator/plugins/images/admin-text-dark.png')}}" alt="home" class="light-logo" />
-                </span>
+            <a href="{{route('admin_home')}}">
+                <img src="{{asset('assets/jump/imgs/jumpq-logo.png')}}" style="object-position: center;" width="50" height="50" alt="home" />
             </a>
         </div>
         <!-- /Logo -->
--- resources/views/includes/navbar1.blade.php
+++ resources/views/includes/navbar1.blade.php
@@ -1,14 +1,10 @@
 <nav class="navbar navbar-default navbar-static-top m-b-0">
     <div class="navbar-header">
-        <div class="top-left-part">
+        <div style="display: flex;align-items:center;justify-content:center" class="top-left-part">
             <!-- Logo -->
-            <a class="logo" href="{{route('staff_home')}}">
-                <!-- Logo icon image, you can use font-icon also --><b>
-                <!--This is dark logo icon--><img src="{{url('assets/administrator/plugins/images/admin-logo.png')}}" alt="home" class="dark-logo" /><!--This is light logo icon--><img src="{{url('assets/administrator/plugins/images/admin-logo-dark.png')}}" alt="home" class="light-logo" />
-             </b>
-                <!-- Logo text image you can use text also --><span class="hidden-xs">
-                <!--This is dark logo text--><img src="{{url('assets/administrator/plugins/images/admin-text.png')}}" alt="home" class="dark-logo" /><!--This is light logo text--><img src="{{url('assets/administrator/plugins/images/admin-text-dark.png')}}" alt="home" class="light-logo" />
-             </span> </a>
+            <a href="{{route('staff_home')}}">
+                <img src="{{asset('assets/jump/imgs/jumpq-logo.png')}}" style="object-position: center;" width="50" height="50" alt="home" />
+            </a>
         </div>
         <!-- /Logo -->
         <!-- Search input and Toggle icon -->
@@ -21,7 +17,8 @@
         <ul class="nav navbar-top-links navbar-right pull-right">
             <li>
                 <form role="search" class="app-search hidden-sm hidden-xs m-r-10">
-                    <input type="text" placeholder="Search..." class="form-control"> <a href=""><i class="fa fa-search"></i></a> </form>
+                    <input type="text" placeholder="Search..." class="form-control"> <a href=""><i class="fa fa-search"></i></a>
+                </form>
             </li>
             <li class="dropdown">
                 <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="{{route('profile_pic',Auth::guard('staff')->user()->profile_image_location)}}" alt="user-img" width="40" height="40" class="img-circle"><b class="hidden-xs">{{Auth::guard('staff')->user()->firstname}}</b><span class="caret"></span> </a>
@@ -29,7 +26,10 @@
                     <li>
                         <div class="dw-user-box">
                             <div class="u-img"><img src="{{route('profile_pic',Auth::guard('staff')->user()->profile_image_location)}}" alt="user" /></div>
-                            <div class="u-text"><h4>{{Auth::guard('staff')->user()->firstname}} {{Auth::guard('staff')->user()->lastname}}</h4><p class="text-muted">{{Auth::guard('staff')->user()->email}}</p></div>
+                            <div class="u-text">
+                                <h4>{{Auth::guard('staff')->user()->firstname}} {{Auth::guard('staff')->user()->lastname}}</h4>
+                                <p class="text-muted">{{Auth::guard('staff')->user()->email}}</p>
+                            </div>
                         </div>
                     </li>
                     <li role="separator" class="divider"></li>
